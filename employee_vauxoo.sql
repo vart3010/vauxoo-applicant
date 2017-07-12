73f17a1d1b05d85c44624bbb19dde5be79e9ab4c
Victor Tortolero <vart10.30@gmail.com>
2017-07-11 19:37:45 -0430

    answer #2

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -6,19 +6,20 @@
 CREATE TABLE employee_department (
 	id SERIAL PRIMARY KEY     NOT NULL,
 	name CHAR(200)  NOT NULL,
-	description  TEXT  NOT NULL
+	description TEXT  NOT NULL
 );
 
 CREATE TABLE employee (
-	id SERIAL PRIMARY KEY     NOT NULL,
-	first_name           CHAR(100)  NOT NULL,
-	last_name           CHAR(100)  NOT NULL,
+	id SERIAL PRIMARY KEY NOT NULL,
+	first_name CHAR(100)  NOT NULL,
+	last_name CHAR(100)  NOT NULL,
 	id_employee_department INTEGER REFERENCES employee_department
 );
 
-
-
 CREATE TABLE employee_hobby (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name CHAR(200) NOT NULL,
+	description TEXT NOT NULL
 );
 
 INSERT INTO employee_department  (name,description) VALUES 
@@ -34,6 +35,29 @@ INSERT INTO employee (first_name,last_name,id_employee_department) VALUES
 ('Andrea','Figueredo','3'),
 ('Ismael','Rodriguez','6'),
 ('Luis','Salazar','5')
+;
+
+INSERT INTO employee_hobby (name,description) VALUES 
+('Musica','Componer e interpretar canciones'),
+('Electronica','Elaborar circuitos'),
+('Programar','desarrollar aplicaciones')
+;
+
+CREATE TABLE employee_hobby_employee  (
+	id_employee INTEGER REFERENCES employee,
+	,id_employee_hobby INTEGER REFERENCES employee_hobby
+);
+
+INSERT INTO employee_hobby_employee (id_employee,id_employee_hobby) VALUES
+('9','1'),
+('9','2'),
+('10','2'),
+('10','3'),
+('11','1'),
+('11','2'),
+('11','3'),
+('12','1'),
+('12','3')
 ;
 
 -- ...
